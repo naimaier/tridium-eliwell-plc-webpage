3b33f467fbbe48da058a5b4272f355855968396a
Henrique Naimaier <coubron@yahoo.com.br>
2021-02-23 14:33:51 -0300

    Refactored header

--- base.js
+++ base.js
@@ -1,10 +1,7 @@
 // Funcion triggered by the responsive menu icon that includes the css selector 
 // that makes menu items to appear vertically
 function responsiveMenu() {
-    var x = document.getElementById("menu");
-        if (x.className === "menu") {
-            x.className += " responsive";
-        } else {
-            x.className = "menu";
-        }
+    var menu = document.querySelector('[data-menu]')
+
+    menu.classList.toggle('responsive')
 }
