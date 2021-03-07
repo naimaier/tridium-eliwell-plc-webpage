d725a6ddfe3d285eb8c27072e92149f63d9f6091
Henrique Naimaier <coubron@yahoo.com.br>
2021-03-07 20:38:57 -0300

    Adding export to xlsx WIP

---
+++ xls/export.js
@@ -0,0 +1,39 @@
+function exportXls() {
+    let zip = new JSZip()
+
+    // ROOT
+    zip.file("[Content_Types].xml", contentTypes)
+    
+    // RELS
+    zip.folder("_rels").file(".rels", rels)
+    
+    // DOCPROPS
+    zip.file("docProps/app.xml", app)
+    zip.file("docProps/core.xml", core)
+
+    // XL
+    let xl = zip.folder("xl")
+    xl.file("sharedStrings.xml", sharedStrings)
+    xl.file("styles.xml", styles)
+    xl.file("workbook.xml", workbook)
+
+    xl.file("_rels/workbook.xml.rels", rels_workbook)
+
+    let drawings = xl.folder("drawings")
+    drawings.file("_rels/drawing1.xml.rels", drawings_rels)
+    drawings.file("drawing1.xml", drawing1)
+
+    // MEDIA
+    xl.file(`media/image1.png`, image1, { base64: true })
+    xl.file(`media/image2.png`, image2, { base64: true })
+    xl.file(`media/image3.png`, image3, { base64: true })
+
+    let worksheets = xl.folder("worksheets")
+    worksheets.file("_rels/sheet1.xml.rels", worksheets_rels)
+    worksheets.file("sheet1.xml", sheet1)
+
+    zip.generateAsync({type:"blob"})
+    .then(function(content) {
+        saveAs(content, "example.xlsx")
+    })
+}
