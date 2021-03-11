2ef01f011097cad883843543fc7d7e8071f40be0
Henrique Naimaier <coubron@yahoo.com.br>
2021-03-11 10:40:19 -0300

    New icon to close mobile menu (x) and size fix

--- base.js
+++ base.js
@@ -1,7 +1,11 @@
 // Funcion triggered by the responsive menu icon that includes the css selector 
 // that makes menu items to appear vertically
 function responsiveMenu() {
-    var menu = document.querySelector('[data-menu]')
+    const menu = document.querySelector('[data-menu]')
 
     menu.classList.toggle('responsive')
+
+    const menuIcon = document.querySelector('[data-menu-icon]')
+    menuIcon.classList.toggle('fa-bars')
+    menuIcon.classList.toggle('fa-times')
 }
